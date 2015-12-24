b49f750a133326971d27d4a68b6215c0ab7e6fdb
Paolo Scanferla <paolo.scanferla@mondora.com>
2015-12-25 00:31:48 +0100

    Fixed logging in wrapping, simplified code

--- src/wrap.js
+++ src/wrap.js
@@ -1,16 +1,10 @@
 export default function wrap (handler) {
-    return (req, res) => {
+    return async (req, res) => {
         try {
-            const ret = handler(req, res);
-            ret.catch && ret.catch(err => {
-                // Handle async exception
-                req.log && req.log(err, "Uncaught exception");
-                res.status(500).send("Internal server error");
-            });
+            await handler(req, res);
         } catch (err) {
-            // Handle sync exception
-            req.log && req.log(err, "Uncaught exception");
             res.status(500).send("Internal server error");
+            req.log && req.log.error(err, "Uncaught exception");
         }
     };
 }

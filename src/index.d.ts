e32d8249fa72fde9e8703fd8ad370e5348a35afe
Paolo Scanferla <paolo.scanferla@gmail.com>
2017-11-19 23:15:23 +0200

    feat: improve TypeScript type definitions

--- src/index.d.ts
+++ src/index.d.ts
@@ -3,51 +3,55 @@
 import { OptionsJson } from "body-parser";
 import { RequestHandler, Router } from "express";
 
-interface IResponseDefinition {
-    description?: string;
-    schema?: any;
-    headers?: any;
-    examples?: any;
-}
-interface IParameter {
-    name: string;
-    description?: string;
-    in: "body" | "path" | "query" | "header";
-    required?: boolean;
-    type?: string;
-    schema?: any;
+declare namespace convexpress {
+    interface IResponseDefinition {
+        description?: string;
+        schema?: any;
+        headers?: any;
+        examples?: any;
+    }
+    interface IParameter {
+        name: string;
+        description?: string;
+        in: "body" | "path" | "query" | "header";
+        required?: boolean;
+        type?: string;
+        schema?: any;
+    }
+
+    interface IConvRoute {
+        path: string;
+        method: string;
+        handler: RequestHandler;
+        parameters?: IParameter[];
+        middleware?: RequestHandler[];
+        description?: string;
+        tags?: string[];
+        responses?: {
+            [statusCode: string]: IResponseDefinition;
+        };
+    }
+
+    interface IConvRouter extends Router {
+        convroute: (route: IConvRoute) => this;
+        serveSwagger: () => this;
+        loadFrom: (pattern: string) => this;
+    }
+
+    interface IOptions {
+        host: string;
+        basePath?: string;
+        info: string;
+        bodyParserOptions?: {
+            limit?: OptionsJson["limit"];
+            strict?: OptionsJson["strict"];
+            verify?: OptionsJson["verify"];
+        };
+    }
 }
 
-interface IConvRoute {
-    path: string;
-    method: string;
-    handler: RequestHandler;
-    parameters?: IParameter[];
-    middleware?: RequestHandler[];
-    description?: string;
-    tags?: string[];
-    responses?: {
-        [statusCode: string]: IResponseDefinition;
-    };
-}
-
-interface IConvRouter extends Router {
-    convroute: (route: IConvRoute) => this;
-    serveSwagger: () => this;
-    loadFrom: (pattern: string) => this;
-}
-
-interface IOptions {
-    host: string;
-    basePath?: string;
-    info: string;
-    bodyParserOptions?: {
-        limit?: OptionsJson["limit"];
-        strict?: OptionsJson["strict"];
-        verify?: OptionsJson["verify"];
-    };
-}
-
-declare function convexpress(options: IOptions): IConvRouter;
+declare function convexpress(
+    options: convexpress.IOptions
+): convexpress.IConvRouter;
 
 export = convexpress;

bf9ddd4adcb0c01d4eeb9cd6b25f9bba15f8adae
pscanf <paolo.scanferla@gmail.com>
2019-04-25 00:59:02 +0300

    docs: fix base url, only publish after qa

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,6 +1,7 @@
 module.exports = {
     title: "convexpress",
     description: "Employ conventions to register express routes",
+    base: "./",
     themeConfig: {
         nav: [
             { text: "Home", link: "/" },
